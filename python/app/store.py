e4c1014ed63223bb9ae86390c3c3549ca4b6b181
gcfbn <bmekarski@interia.pl>
2022-06-18 18:19:36 +0200

    Add creating a product.

--- python/app/store.py
+++ python/app/store.py
@@ -3,7 +3,7 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 
 from .dao.store_dao import get_user_cart, remove_from_cart as dao_remove_from_cart, change_quantity_in_cart, \
     create_purchase, add_item_to_purchase, set_current_availability, get_single_item, add_to_cart as dao_add_to_cart, \
-    already_in_cart, get_items, get_items_sold_by, update_item_price
+    already_in_cart, get_items, get_items_sold_by, update_item_price, add_product
 
 store = Blueprint('store', __name__)
 
@@ -115,3 +115,16 @@ def change_availability():
 @store.route('/add_item')
 def add_item():
     return render_template('add_item.html')
+
+
+@store.route('/add_item', methods=['POST'])
+def add_item_post():
+    user_id = flask_login.current_user.id
+    name = request.form['productName']
+    description = request.form['productDescription']
+    price = request.form['productPrice']
+    availability = request.form['productAvailability']
+    unit = request.form['productUnit']
+    add_product(price, name, description, availability, unit, user_id)
+    flash('Product added.')
+    return redirect(url_for('store.sold_items'))
